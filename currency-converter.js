33bf8b3119ad0e001224ab13274f7252a45f2bad
Trifon Trifonov <trifon.trifonov@solarwinds.com>
2019-06-14 18:19:49 +0300

    Integrate fixer Api for live currency update

--- currency-converter.js
+++ currency-converter.js
@@ -1,46 +1,95 @@
 
-// var CurrencyConverter = (function(){
-//   var 
-var currencies = {
-  'EUR': {'SFR': 1.3135, 'USD': 1.2897, 'BPD': 0.8631},
-  'USD': {'JPY': 109.6200},
-  'SFR': {'USD': 0.9960},
-  'BPD': {'CAD': 1.7574}
-};
 
-var calculateButton = document.querySelector('.calculate-currency');
-var baseCurrencyInput = document.getElementById('currency-from');
-var secondCurrencyInput = document.getElementById('currency-to');
-var amountInput = document.getElementById('currency-amount');
-var toShowAmount = document.querySelector('.given-amount');
-var toShowBase = document.querySelector('.base-currency');
-var toShowSecond = document.querySelector('.second-currency');
-var toShowResult = document.querySelector('.calculated-currency');
+// const currencies = {
+//   EUR: { SFR: 1.3135, USD: 1.2897, BPD: 0.8631 },
+//   USD: { JPY: 109.6200 },
+//   SFR: { USD: 0.9960 },
+//   BPD: { CAD: 1.7574 }
+// }
+// getSupportedSymbols()
 
-function convertCurrency(event) {
-  event.preventDefault();
-  var amount = amountInput.value;
-  var from = baseCurrencyInput.value;
-  var to = secondCurrencyInput.value;
-  var result = 0;
+let calculatedAmount = document.getElementById('calculated-amount')
+
+function getFormData() {
+  var amount, from, to;
+  amount = document.getElementById('amount')
+  if (amount) amount = amount.value
   
-  try{
-    if (from == to){
-      result = amount;
-    } else {
-     result = amount * currencies[from][to];
-  }
-  }
-  catch(err) {
-    result = amount * (1 / currencies[to][from]);
-  }
+  from = document.getElementById('from')
+  if (from) from = from.value
   
-  toShowAmount.innerHTML = amount;
-  toShowBase.textContent = from + ' = ';
-  toShowSecond.textContent = to;
-  toShowResult.textContent = result; 
+  to = document.getElementById('to')
+  if (to) to = to.value
+  return {amount, from, to}
 }
 
-calculateButton.addEventListener('click', convertCurrency);
+// function getSupportedSymbols() {
+//   const xhr = new XMLHttpRequest();
+//   const apiKey = 'symbols?access_key=789b6637e9d33c71debd4b01b758a5e1'
+//   const server = `http://data.fixer.io/api/${apiKey}`
 
-// })();
+//   xhr.open('GET', server, true);
+//   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+//   xhr.onreadystatechange = function() {
+//       if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
+//       var response = JSON.parse(this.response);
+//       if (response && response.success) 
+//         populateDropdowns(response);
+//       else if (response && response.error && response.error.type)
+//         alert(response.error.type)
+//   }
+//   xhr.send()
+// }
+
+// function populateDropdowns(response) {
+//   debugger
+//   fromSelect = document.getElementById('from')
+//   toSelect = document.getElementById('to')
+//   var i = 0;
+
+//   for (currency in response.symbols) {
+//     //  var opt = document.createElement("option")
+//     //  opt.value = i
+//     //  opt.innerHTML = currency
+//     //  fromSelect.appendChild(opt);
+//     //  toSelect.appendChild(opt);
+//     //  i++
+
+//     fromSelect.options[fromSelect.options.length] = new Option(currency);
+//     toSelect.options[fromSelect.options.length] = new Option(currency);
+//   }
+
+// }
+
+function getCurrencyExchangeRates(event) {
+  event.preventDefault();
+  const fD = getFormData();
+  const xhr = new XMLHttpRequest();
+  const apiKey = 'latest?access_key=789b6637e9d33c71debd4b01b758a5e1'
+  const queryParams = `&base=${fD.from}&symbols=EUR,JPY,GBP`
+  const server = `http://data.fixer.io/api/${apiKey + queryParams}`
+
+  xhr.open('GET', server, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+  xhr.onreadystatechange = function() {
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200)
+      var response = JSON.parse(this.response);
+      if (response && response.success) 
+        showConversion(response);
+      else if (response && response.error && response.error.type)
+        alert(response.error.type)
+  }
+  xhr.send()
+}
+
+function showConversion(response) {
+  debugger
+  const fD = getFormData();
+
+  let calculatedConversion = 0;
+  if (fD.from == fD.to) calculatedConversion = fD.amount
+  else calculatedConversion = parseInt(fD.amount).toFixed(2) * response.rates[fD.to].toFixed(2);
+  calculatedAmount.innerHTML = `${fD.amount} ${fD.from} is ${calculatedConversion} ${fD.to}`
+}
+
+
